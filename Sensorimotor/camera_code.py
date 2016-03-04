77ffde78346c437b51877d2d37b088de1f0038b2
N.B <niburka@vassar.edu>
2016-03-04 13:25:20 -0500

    Sensorimotor milestone

--- Sensorimotor/camera_code.py
+++ Sensorimotor/camera_code.py
@@ -1,4 +1,6 @@
 import math
+#import numpy
+from kovan import *
 
 average_black_blob = 0
 stderr_black_blob = 0
@@ -18,7 +20,9 @@ def calculateStdError(list_of_vals, average):
 def calculateBlackBlobs():
     # These are global variables that this func will modify
     global average_black_blob, stderr_black_blob
-    camera_open(LOW_RES)
+    print("no camera")
+    camera_open_at_res("LOW_RES")
+    print("camera open")
     # Will store a list of num_samples averages
     # Will themselves be averaged to calculate final mean
     average_black_blobs_list = []
@@ -33,7 +37,10 @@ def calculateBlackBlobs():
         for i in range(0, object_count):
             sum_area_black_blobs += get_object_area(black_channel, i)
         # calculates and stores average total area takwn up by blobs
-        average_black_blobs.append(sum_area_black_blobs/ object_count)
+        if (not (object_count == 0)):
+            average_black_blobs_list.append(sum_area_black_blobs/ object_count)
+        else: 
+            print "NO OBJECTS!"
         num_samples -= 1
         #Sleep for 1 second. Will allow the bot to move around the room
         # and take samples
@@ -41,6 +48,8 @@ def calculateBlackBlobs():
         print "Average black blobs in sample", average_black_blobs_list
         msleep(1000)
     average_black_blob = sum(average_black_blobs_list)/float(len(average_black_blobs_list))
-    stderr_black_blob = calculateStdError(average_black_blobs_list, average_black_blob)
+    stderr_black_blob = calculateStdError(average_black_blobs_list, average_black_blob)    	  
+    camera_close()
+    return average_black_blob, stderr_black_blob
 
-print ("Average saved black blob: %s\nStd err: %s " %  (average_black_blob, stderr_black_blob)) 
+#print ("Average saved black blob: %s\nStd err: %s " %  (average_black_blob, stderr_black_blob)) 
---
+++ Sensorimotor/main.py
@@ -0,0 +1,79 @@
+# Created on Fri February 26 2016
+
+from kovan import *
+import camera_code
+
+#numbers of the IR sensors
+IR_left = 0
+IR_right = 1
+IR_fwd = 2
+
+#numbers of motor ports
+motor_left = 1
+motor_right = 3
+
+#clear motors
+#clear_motor_position_counter(motor_left)
+#clear_motor_position_counter(motor_right)
+
+#threshold for the IR sensors
+IR_thresh = 280
+
+#number of milliseconds between each loop
+tick = 50
+
+speed = 85u
+
+
+def fwd():
+	motor(motor_left, speed)
+	motor(motor_right, speed)
+	
+def turn_right(in_place=True):
+	#if in place, opposite motor goes backward
+	if in_place:
+		motor_right_vel = -1*speed
+	else:
+		motor_right_vel = 40
+	motor(motor_left, speed)
+	motor(motor_right, motor_right_vel)
+
+def turn_left(in_place=True):
+	if in_place:
+		motor_left_vel = -1*speed
+	else:
+		motor_left_vel = 40
+	motor(motor_left, motor_left_vel)
+	motor(motor_right, speed)
+	
+'''
+average_black_blob = 0
+stderr_black_blob = 0
+black_channel = 0 #
+average_black_blob, stderr_black_blob = camera_code.calculateBlackBlobs()
+print "av: ", average_black_blob
+print "std: ", stderr_black_blob
+
+'''	
+while(True):
+	#this variable indicates whether or not an action runs for a specific amount of time
+	diff_time_for_action = False
+	left = analog_et(IR_left)
+
+	#if there's something left, turn right
+	if left > IR_thresh:
+		print "TURNING RIGHT! IR_LEFT: ", left 
+		turn_right()
+	#else if there's something right, turn left
+	elif analog_et(IR_right) > IR_thresh:
+		turn_left()
+	#else go forward
+	else:
+		print "FORWARD!"
+		fwd()
+		
+	#do selected action for tick num of milliseconds, unless the selected action
+	#runs for a specific amount of time different than tick
+	if not diff_time_for_action:
+		msleep(tick)
+
