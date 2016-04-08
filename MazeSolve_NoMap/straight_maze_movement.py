850aa35a36fbe1fb641f3c492ab4e93e924c57a7
Jake Brawer <jabrawer@vassar.edu>
2016-04-08 12:50:55 -0400

    try this

--- MazeSolve_NoMap/straight_maze_movement.py
+++ MazeSolve_NoMap/straight_maze_movement.py
@@ -16,10 +16,10 @@ def stay_mid():
     left_IR_val = analog_et(IR_left)
     right_IR_val = analog_et(IR_right)
     print('Left IR: %s,  Right IR: %s' % (left_IR_val, right_IR_val))
-    if(left_IR_val > mid_thresh ):
-        motor(motor_right,70*(left_IR_val/800))
-    if(right_IR_val > mid_thresh):
-        motor(motor_left,70*(right_IR_val/800))
+    #if(left_IR_val > mid_thresh ):
+    motor(motor_right,70*(left_IR_val/800))
+    #if(right_IR_val > mid_thresh):
+    motor(motor_left,70*(right_IR_val/800))
 
 
 while(True):
